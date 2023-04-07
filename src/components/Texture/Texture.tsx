c9c3f6b57a3f9d04a6c27ad633e163e93149450f
Alexandr Yushenko <bocsyr.516@gmail.com>
2023-04-08 00:20:30 +0600

    upd 08.04

--- src/components/Texture/Texture.tsx
+++ src/components/Texture/Texture.tsx
@@ -1,7 +1,7 @@
 import c from "./Texture.module.scss";
-import texture from "src/assets/img/unnamed.jpg";
+// import texture from "src/assets/img/unnamed.jpg";
 import React from "react";
 
-const Texture = () => <div className={c.texture}><img src={texture} alt=""/></div>
+const Texture = () => <div className={c.texture}><img src={'src/assets/img/unnamed.jpg'} alt=""/></div>
 
 export default Texture
