2abe3b3b478e05660a0129dbd4761b965bfb8029
ANTAGONIST <85052188+ANT9996@users.noreply.github.com>
2023-04-18 22:28:33 +0600

    Update axios.ts

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:5000"
+    baseURL: "https://quizzz-react-backend.onrender.com/"
 })
 
 instance.interceptors.request.use((config) => {
