7f66490df8cb9506e6e22488633be9234aa52340
Alexandr <bocsyr.516@gmail.com>
2023-10-15 00:18:11 +0600

    Уведомления при успехе/ошибке

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,8 @@
 import {create} from "zustand";
 import {User} from "./types";
 import axios from "./axios";
+import {NotificationManager} from "react-notifications";
+import {ADD_QUEST_SUCCESS} from "./constants";
 
 interface Store {
   user: User | null
@@ -20,13 +22,19 @@ export const useStore = create<Store>((set) => ({
       const {data} = await axios.post('/auth/login', params)
       window.localStorage.setItem('token', data.token)
       set(() => ({user: data}))
+      NotificationManager.success('Вы успешно авторизовались')
       return data
     },
     register: async (params) => {
-    const {data} = await axios.post('/auth/user', params)
-      window.localStorage.setItem('token', data.token)
-      set(() => ({user: data}))
-      return data
+      try {
+        const response = await axios.post('/auth/user', params)
+        window.localStorage.setItem('token', response.data.token)
+        set(() => ({user: response.data}))
+        NotificationManager.success('Вы успешно зарегистрировались')
+        return response.data
+      } catch (e) {
+        NotificationManager.error('Не удалось зарегистрироваться')
+      }
     },
     logout: () => {
       window.localStorage.removeItem('token')
